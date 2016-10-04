ec3b686aac5ed71482591b19ccdd4601dcb77ddc
newtork <casio-fraction@web.de>
2016-10-04 16:09:06 +0200

    explaining the "idea"

--- challenges/closestSequence2/solution.closestSequence2.js
+++ challenges/closestSequence2/solution.closestSequence2.js
@@ -1,3 +1,17 @@
+/*
+	Idea:
+	 * Take algorithm "Dynamic Time Warp" as template
+	 * Do not allow sequence "a" to warp smaller / compress,
+	   the depending cost would be infinite
+	 * Draw distance matrix between the two sequences for the example.
+	   Observe the "closestSequence" by following the least-steep distance path.
+	   You are allowed to walk diagonally = "take number b_i" or vertically "skip number b_i"
+	 * Use dynamic programming to improve performance:
+	   Do not fill whole matrix, you can succeed by just keeping the current b_i "Dcurrent" row
+	   and the previous b_(i-1) "Dprev" row. Use a seperate array "DistanceUntil" to store the
+	   best accumulated cost.
+*/
+
 function closestSequence2(a, b) {
 	var d = function(p,q) {
 		return Math.abs(p-q);
